af81b56a46761afa94e0a02130433167a74ac887
Dany <dannismeh@gmail.com>
2024-11-28 20:45:35 +0700

    Add files via upload

--- Backend/Controller/Controller.java
+++ Backend/Controller/Controller.java
@@ -1,56 +1,51 @@
-package com.pdm.farming.Controller;
-
-import com.pdm.farming.Entities.Account;
-import com.pdm.farming.Service.AccountService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+package com.pdm.farming.Layers;
 import java.util.List;
-import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pdm.farming.Entities.Farmer;
 
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/api/farmers")
 public class Controller {
 
-    @Autowired
-    private AccountService accountService;
+    private final FarmerService farmerService;
+
+    public Controller(FarmerService farmerService) {
+        this.farmerService = farmerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Farmer> createFarmer(@RequestBody Farmer farmer) {
+        return ResponseEntity.ok(farmerService.createFarmer(farmer));
+    }
 
     @GetMapping
-public ResponseEntity<List<Account>> getAllAccounts() {
-    List<Account> accounts = accountService.getAllAccounts();
-    return ResponseEntity.ok(accounts);
-}
-
-    // Create an account
-    @PostMapping
-    public Account createAccount(@RequestBody Account account) {
-        return accountService.createAccount(account);
+    public ResponseEntity<List<Farmer>> getAllFarmers() {
+        return ResponseEntity.ok(farmerService.getAllFarmers());
     }
 
-    // Get account by username
-    @GetMapping("/username/{username}")
-    public Optional<Account> getAccountByUsername(@PathVariable String username) {
-        return accountService.getAccountByUsername(username);
+    @GetMapping("/{id}")
+    public ResponseEntity<Farmer> getFarmerById(@PathVariable Long id) {
+        return ResponseEntity.ok(farmerService.getFarmerById(id));
     }
 
-    // Get account by email
-    @GetMapping("/email/{email}")
-    public Optional<Account> getAccountByEmail(@PathVariable String email) {
-        return accountService.getAccountByEmail(email);
+    @PutMapping("/{id}")
+    public ResponseEntity<Farmer> updateFarmer(@PathVariable Long id, @RequestBody Farmer farmer) {
+        return ResponseEntity.ok(farmerService.updateFarmer(id, farmer));
     }
 
-    // Delete an account
-    @DeleteMapping("/{accountId}")
-    public void deleteAccount(@PathVariable Long accountId) {
-        accountService.deleteAccount(accountId);
-    }
-
-    // Update an account
-    @PutMapping("/{accountId}")
-    public Account updateAccount(@PathVariable Long accountId, @RequestBody Account updatedAccount) {
-        return accountService.updateAccount(accountId, updatedAccount);
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFarmer(@PathVariable Long id) {
+        farmerService.deleteFarmer(id);
+        return ResponseEntity.noContent().build();
     }
 }
